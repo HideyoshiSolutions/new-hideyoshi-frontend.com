1b5135e08dec2de570fab51730a0a85e71dc790d
Vitor Hideyoshi Nakazone Batista <vitor.h.n.batista@gmail.com>
2024-07-15 05:15:42 -0300

    Dark Theme Config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, HostBinding, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HlmButtonDirective} from "@spartan-ng/ui-button-helm";
+import {ThemeService} from "../libs/services/theme.service";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HlmButtonDirective],
+    host: {
+        class:
+            'block h-full bg-tertiary-50 ',
+    },
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'new-frontend-hideyoshi';
+    title = 'new-frontend-hideyoshi';
+
+    @HostBinding('class.dark:bg-primary-900')
+    isDarkMode = false;
+
+    constructor(private _themeService: ThemeService) {
+        this._themeService.theme$.subscribe((theme) => {
+            this.isDarkMode = theme === 'dark';
+        });
+    }
+
+    public toggleTheme(): void {
+        this._themeService.toggleDarkMode();
+    }
 }
