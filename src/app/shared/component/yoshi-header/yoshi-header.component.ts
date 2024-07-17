335f0ab8f59618c0070749a2a2febd947f7664d2
Vitor Hideyoshi Nakazone Batista <vitor.h.n.batista@gmail.com>
2024-07-16 22:47:54 -0300

    Adds Icons and Sets Headers

---
+++ src/app/shared/component/yoshi-header/yoshi-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yoshi-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './yoshi-header.component.html',
+  styleUrl: './yoshi-header.component.css'
+})
+export class YoshiHeaderComponent {
+
+}
